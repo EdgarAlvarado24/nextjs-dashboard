e811ac787a2a59dcf46fea3c1cf144966f6990a6
Edgar Alvarado <edgar.alvarado.16@gmail.com>
2026-03-30 17:10:15 -0400

    improving accessibility

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,28 +9,59 @@ const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'})
 
 const FormSchema = z.object({
     id:z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
+    customerId: z.string({
+      invalid_type_error: 'Please select a customer'
+    }),
+    amount: z.coerce
+      .number()
+      .gt(0, {message: 'Please enter an amount greater than 80.'}),
     date: z.string(),
-    status:z.enum(['pending', 'paid']),
+    status:z.enum(['pending', 'paid'], {
+      invalid_type_error: 'Please select an invoice status.'
+    }),
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const {customerId, amount, status} = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
-    console.log(customerId)
+    console.log(validatedFields)
+
+    if(!validatedFields.success){
+      return{
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice. ',
+      };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try{
+      await sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+    } catch(error){
+      console.log(error);
+      return {
+        message: 'Database Error: Failed to Create Invoice.'
+      }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -50,11 +81,18 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try{
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch(error){
+    console.log(error)
+    return {
+      message:'Database Error: Failed to Update Invoice.'
+    }
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+// eslint.config.mjs
+import nextConfig from 'eslint-config-next';
+
+const eslintConfig = [
+  ...nextConfig,
+  {
+    ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+];
+
+export default eslintConfig;
